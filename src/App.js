00ca487aae56680f20a66349c014af64612b5c91
Manoranjan <manoranjan.mishra.mail@gmail.com>
2022-09-29 08:46:15 +0530

    hello

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
@@ -9,14 +7,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
           Learn React
-        </a>
       </header>
     </div>
   );
